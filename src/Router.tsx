5c99cfeeb290375266d63b397e41a5358a71852a
minseo-hong <mshong1007@gmail.com>
2023-11-01 23:44:44 +0900

    feat: SideNavBar 컴포넌트 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
+import SideNavBar from './components/SideNavBar';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <SideNavBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/components/SideNavBar.tsx
@@ -0,0 +1,5 @@
+const SideNavBar = () => {
+  return <div>사이드 네비게이션 바 입니다.</div>;
+};
+
+export default SideNavBar;
